ebf4f48f4089281d97b0e7531c2588e09f3fa7fd
Hojin Jang <jhojin7@gmail.com>
2023-04-05 17:05:20 +0900

    .

--- users/app.py
+++ users/app.py
@@ -1,26 +1,16 @@
 from fastapi import FastAPI
 import os
-# from db import get_session
+from db import get_session
 
-# app = FastAPI(host="0.0.0.0", port="5556", )
-app = FastAPI()
+app = FastAPI(host="0.0.0.0", port="5556", )
+# app = FastAPI()
 
 @app.get("/")
 def root():
-    return "hello from users service"
-
-@app.get("/dbinfo")
-def db_info():
-    # session = get_session()
-    db_url = os.getenv("DB_URL")
-    db_name = os.getenv("DB_NAME")
-    db_port = os.getenv("DB_PORT")
-    db_username = os.getenv("DB_USERNAME")
-    # db_password
-
+    session = get_session()
     return \
     f"""
-    {db_url,db_name,db_port,db_username}
-    """ 
+    return "hello from users service"
     # {session}
     # {session.info}
+    """ 
--- users/db.py
+++ users/db.py
@@ -6,88 +6,42 @@ from sqlalchemy import URL
 import os
 
 Base = declarative_base()
-
-class Person(Base):
-    __tablename__ = "people"
-    ssn = Column("ssn",Integer,primary_key=True)
-    firstname = Column("firstname",String)
-    lastname = Column("lastname",String)
-    gender = Column("gender",CHAR)
-    age = Column("age",Integer)
-
-    def __init__(self, ssn, first, last,gender,age) -> None:
-        self.ssn = ssn
-        self.firstname = first
-        self.lastname = last
-        self.gender = gender
-        self.age = age
-    
-    def __repr__(self):
-        return f"Person({self.firstname}, {self.lastname}, {self.gender}, {self.age})"
-
-class Thing(Base):
-    __tablename__ = "things"
-    tid = Column("tid",Integer,
-        primary_key=True, autoincrement=True,
-        unique=True)
-    description = Column("description",String)
-    owner = Column(Integer,ForeignKey("people.ssn"))
-
-    def __init__(self,desc,owner) -> None:
-        # self.tid = tid
-        self.description = desc
-        self.owner = owner
-
-    def __repr__(self):
-        return f"Thing({self.tid}, {self.description}, {self.owner})"
-
-
 def get_session():
-    db_url = os.getenv("DB_URL")
-    db_name = os.getenv("DB_NAME")
-    db_port = os.getenv("DB_PORT")
-    db_username = os.getenv("DB_USERNAME")
-    # # f"mariadb+pymysql://{db_username}:{os.getenv('DB_PASSWORD')}@{db_url}/{db_name}?charset=utf8mb4",
+    # # docker_name = "sqlalchemy-database-1"
+    # username = "prof1"
+    # password = "prof1!"
+    # databasename = "exampledb"
+    # port = 3306
+    # full_url = f"mariadb+pymysql://{username}:{password}@{}:{}/exampledb?charset=utf8mb4"
+    # # full_url = f"mysql+mysqlconnector://{user}:{pw}@{docker_name}:{port}/{databasename}?charset=utf8mb4"
+    # engine = create_engine(full_url)
+
     url_object = URL.create(
         "mariadb+pymysql",
-        username=os.getenv("DB_USERNAME"),
-        password=os.getenv("DB_PASSWORD"),  # plain (unescaped) text
-        host=os.getenv("DB_URL"),
-        database=os.getenv("DB_NAME"),
+        # "mysql+mysqlconnector",
+        # username = "prof1",password = "prof1!",
+        username = "root",password = "password",
+        # host=os.getenv("DB_URL"),
+        # database=os.getenv("DB_NAME"),
+        host = "capstonedb",
+        database = "db",
     )
-    engine = create_engine(url_object,echo=True)
+    engine = create_engine(url_object,echo=False)
 
     Base.metadata.create_all(bind=engine)
+    return sessionmaker(bind=engine)
 
-    Session = sessionmaker(bind=engine)
-    session = Session()
-    return session
+print(get_session())
+# def print_all():
+#     session = get_session()
+#     print(session.query(Person).all())
+#     print(session.query(Thing).all())
 
-session = get_session()
+# def person_things():
+#     session = get_session()
+#     res = session.query(Thing,Person)\
+#         .filter(Thing.owner==Person.ssn)\
+#         .filter(Person.firstname=="a1")
+#     return [r for r in res]
 
-# people = [
-#     Person(123,"a1","b1","m",12),
-#     Person(456,"a2","b2","f",22),
-#     Person(789,"a3","b3","m",19),
-# ]mysql
-# for p in people:
-#     session.add(p)
-
-# session.add(Thing(desc="Thing1",owner=123))
-# session.add(Thing(desc="Thing2",owner=456))
-# session.add(Thing(desc="Thing3",owner=123))
-
-# session.commit()
-print(session.query(Person).all())
-print(session.query(Thing).all())
-
-# res = session.query(Person).filter(Person.age>20)
-# res = session.query(Person).filter(Person.firstname.like("%a%"))
-
-# res = session.query(Thing).all()
-def person_things():
-    res = session.query(Thing,Person)\
-        .filter(Thing.owner==Person.ssn)\
-        .filter(Person.firstname=="a1")
-    return res
-# """
+# print(person_things())
